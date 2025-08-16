5713863a9f42bdd6d19062a639fc1e08869ac728
JaimeCamachoDev <jaime.camacho.dev@gmail.com>
2025-08-17 01:59:07 +0200

    updated

--- Assets/1-Scripts/DOTS/Templates/Movement/Components/MovementPathElement.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Entities;
-using Unity.Mathematics;
-
-/// Buffer que almacena el camino actual calculado para la plantilla de movimiento.
-[InternalBufferCapacity(16)]
-public struct MovementPathElement : IBufferElementData
-{
-    public int2 Cell;
-}
